ad8147436223ddeff7f189834ed06b11622421ef
mohamad2225 <mmdmohamad2007@gmail.com>
2025-01-08 15:57:06 +0330

    new gap game

---
+++ offline.js
@@ -0,0 +1,23 @@
+﻿{
+	"version": 1736265958,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/coin-sheet0.png",
+		"images/wallet_icon-sheet0.png",
+		"images/random_box-sheet0.png",
+		"images/box-sheet0.png",
+		"images/exit-sheet0.png",
+		"images/ranking_icon-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"SweetAlert.css",
+		"SweetAlert.js"
+	]
+}
